37f6189c7906b94c5b1f7ab511b65097eba1f2fc
benjamin <b.james.neustadt@gmail.com>
2022-08-04 13:58:35 +0200

    WIP:Modified: homepage route
    * to test for heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use("/bootstrap", express.static(path.join(__dirname + "/node_modules/bootstrap/dist/css")))
 
 
-app.get("/openbook", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
